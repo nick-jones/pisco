698a7a64ddadca31f20ca2a6f45ce21f88783484
Nicholas Jones <me@nicholasjon.es>
2014-02-22 12:46:18 +0000

    Thrift IDL updated, lookup details are now provided in a "Query" struct.
    This allows for more flexible changes, and will stop the "lookup" function arguments
    ballooning any further. Added stubs for features to come.

--- def/pisco.thrift
+++ def/pisco.thrift
@@ -13,8 +13,17 @@ struct Result {
   2: optional i32 total = 0
 }
 
+struct Query {
+  1: optional set<string> include_patterns,
+  2: optional set<string> exclude_patterns,
+  3: optional i32 limit = 10,
+  4: optional bool total_required = false,
+  5: optional bool reverse_order = false,
+  6: optional bool included_deleted = false
+}
+
 service Search {
-  Result lookup(1: string pattern, 2: i32 limit = 10, 3: bool total_required = false),
+  Result lookup(1: Query query),
   bool add(1: Item item),
   bool replace(1: Item item),
   bool remove(1: i32 id)
